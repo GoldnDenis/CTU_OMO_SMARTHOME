d8f04c63581f1f31df2ea99d4ebffbf82752c860
akanomus <akanomus@fel.cvut.cz>
2023-12-29 15:59:06 +0100

    Added some logic to observer in Sensor class.

--- src/main/java/cvut/fel/omo/sensor/Sensor.java
+++ src/main/java/cvut/fel/omo/sensor/Sensor.java
@@ -1,6 +1,12 @@
 package cvut.fel.omo.sensor;
 
+import cvut.fel.omo.event.GLOBAL_EVENT;
+import cvut.fel.omo.system.Logging;
+import cvut.fel.omo.system.RandomGenerator;
+import java.util.logging.Level;
+
 public class Sensor {
+
     public GlobalEventDetector detector;
 
     public Sensor() {
@@ -8,8 +14,54 @@ public class Sensor {
     }
 
     public void tryToDetect() {
-        /* todo: some logic of probability generation
-         if some event occurs, then notify all appliances to react
-         */
+        if (RandomGenerator.hasHappened(10)) {
+            detector.notifyAll(GLOBAL_EVENT.ELECTRICITY_SHUT_OFF);
+            Logging.log(
+                    Level.INFO,
+                    "The electricity shut off has been detected.",
+                    this.toString()
+            );
+            return;
+        }
+        if (RandomGenerator.hasHappened(10)) {
+            detector.notifyAll(GLOBAL_EVENT.WATER_SHUT_OFF);
+            Logging.log(
+                    Level.INFO,
+                    "The water shut off has been detected.",
+                    this.toString()
+            );
+        }
+        if (RandomGenerator.hasHappened(10)) {
+            detector.notifyAll(GLOBAL_EVENT.NON_SATISFYING_TEMP);
+            Logging.log(
+                    Level.INFO,
+                    "Non satisfying temperature has been detected.",
+                    this.toString()
+            );
+        }
     }
+
+    public void detectNightFall() {
+        detector.notifyAll(GLOBAL_EVENT.NIGHT_FELL);
+        Logging.log(
+                Level.INFO,
+                "Night fall has been detected.",
+                this.toString()
+        );
+    }
+
+    public void detectSunRise() {
+        detector.notifyAll(GLOBAL_EVENT.SUN_HAS_RISEN_UP);
+        Logging.log(
+                Level.INFO,
+                "Sun rise has been detected.",
+                this.toString()
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "Global sensor";
+    }
+
 }
